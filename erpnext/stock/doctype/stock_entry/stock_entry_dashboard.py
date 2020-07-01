5706eab5a6acd42604f0f60b938f19d860b6c63f
Kartik Sharma <kartiksharma9319@gmail.com>
2020-07-01 12:40:49 +0000

    feat: Create reference field in Stock Entry (STE) to link STE with PINV, PO, or JV

---
+++ erpnext/stock/doctype/stock_entry/stock_entry_dashboard.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return {
+		'fieldname': 'stock_entry',
+		'non_standard_fieldnames': {
+			'Journal Entry': 'ref_name',
+		},
+		'internal_ref_type_links': {
+			'Purchase Order': ['stock_entry_ref', 'ref_name'],
+			'Journal Entry': ['stock_entry_ref', 'ref_name'],
+			'Purchase Invoice': ['stock_entry_ref', 'ref_name'],
+		},
+		'transactions': [
+			{
+				'label': _('Payment'),
+				'items': ['Journal Entry']
+			},
+			{
+				'label': _('References'),
+				'items': ['Purchase Order', 'Purchase Invoice']
+			}
+		]
+	}
