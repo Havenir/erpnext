db4685aa9956fa4eeeaae9da81360b59e32e2dc1
Kartik Sharma <kartiksharma9319@gmail.com>
2020-08-28 07:47:14 +0000

    fix: #79

--- erpnext/stock/doctype/stock_entry/stock_entry_dashboard.py
+++ erpnext/stock/doctype/stock_entry/stock_entry_dashboard.py
@@ -10,6 +10,7 @@ def get_data():
 		'internal_ref_type_links': {
 			'Purchase Order': ['stock_entry_ref', 'ref_name'],
 			'Purchase Invoice': ['stock_entry_ref', 'ref_name'],
+			'Stock Entry': ['stock_entry_ref', 'ref_name'],
 		},
 		'transactions': [
 			{
@@ -18,7 +19,7 @@ def get_data():
 			},
 			{
 				'label': _('References'),
-				'items': ['Purchase Order', 'Purchase Invoice']
+				'items': ['Purchase Order', 'Purchase Invoice', 'Stock Entry']
 			}
 		]
 	}
