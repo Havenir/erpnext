7c888831963f1aa013c22196481926ee56c134fe
Ahmad <mohammad.ahmad.zulfiqar1@gmail.com>
2022-07-05 13:20:56 +0500

    fix: asset - depreciation schedule for existing assets

--- erpnext/assets/doctype/asset/asset.py
+++ erpnext/assets/doctype/asset/asset.py
@@ -1120,8 +1120,12 @@ def get_depreciation_amount(asset, depreciable_value, row):
 		# if the Depreciation Schedule is being prepared for the first time
 		if not asset.flags.increase_in_asset_life:
 			depreciation_amount = (
-				flt(asset.gross_purchase_amount) - flt(row.expected_value_after_useful_life)
-			) / flt(row.total_number_of_depreciations)
+				flt(asset.gross_purchase_amount)
+				- flt(asset.opening_accumulated_depreciation)
+				- flt(row.expected_value_after_useful_life)
+			) / (
+				flt(row.total_number_of_depreciations) - flt(asset.number_of_depreciations_booked)
+			)
 
 		# if the Depreciation Schedule is being modified after Asset Repair
 		else:
